6c27989a345cf1603a7c23764a77575520ce7b11
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-06-09 12:33:16 +0200

    feat(vm): add State struct

--- crates/runner/src/vm/mod.rs
+++ crates/runner/src/vm/mod.rs
@@ -1,1 +1,19 @@
 pub mod instructions;
+
+use num_traits::One;
+use stwo_prover::core::fields::m31::M31;
+
+#[derive(Clone, Copy, Debug)]
+pub struct State {
+    fp: M31,
+    pc: M31,
+}
+
+impl State {
+    pub fn advance(self) -> Self {
+        Self {
+            fp: self.fp,
+            pc: self.pc + M31::one(),
+        }
+    }
+}
