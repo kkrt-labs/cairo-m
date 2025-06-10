260f7b1270cf4367800070a88bff6d53349d925f
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-06-10 09:17:28 +0200

    refactor(vm): remove register update function for now

--- crates/runner/src/vm/mod.rs
+++ crates/runner/src/vm/mod.rs
@@ -1,26 +1,21 @@
 pub mod instructions;
 
-use num_traits::One;
 use stwo_prover::core::fields::m31::M31;
 
+/// The state of the VM, updated at each step.
+///
+/// * `pc` is the program counter, pointing to the current instruction.
+/// * `fp` is the frame pointer, pointing to the current frame.
 #[derive(Clone, Copy, Debug, Default)]
+#[allow(dead_code)]
 pub struct State {
     fp: M31,
     pc: M31,
 }
 
-impl State {
-    pub fn advance(self) -> Self {
-        Self {
-            fp: self.fp,
-            pc: self.pc + M31::one(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use num_traits::{One, Zero};
+    use num_traits::Zero;
     use stwo_prover::core::fields::m31::M31;
 
     use crate::vm::State;
@@ -30,13 +25,5 @@ mod tests {
         let state = State::default();
         assert_eq!(state.fp, M31::zero());
         assert_eq!(state.pc, M31::zero());
-    }
-
-    #[test]
-    fn test_state_advance() {
-        let state = State::default();
-        let new_state = state.advance();
-        assert_eq!(new_state.fp, M31::zero());
-        assert_eq!(new_state.pc, M31::one());
     }
 }
