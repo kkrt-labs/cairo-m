221a37e7f04f555c417d74acc9b1ece1ea08e151
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-06-17 17:02:54 +0200

    add previous value

--- crates/prover/src/adapter/instructions.rs
+++ crates/prover/src/adapter/instructions.rs
@@ -1,7 +1,7 @@
 use stwo_prover::core::fields::qm31::QM31;
 
-use super::memory::MemoryCache;
 use super::io::{MemEntry, TraceEntry};
+use super::memory::MemoryCache;
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct VmState {
@@ -12,53 +12,52 @@ pub struct VmState {
 impl From<TraceEntry> for VmState {
     fn from(entry: TraceEntry) -> Self {
         Self {
-            pc: entry.pc as u32,
-            fp: entry.fp as u32,
+            pc: entry.pc,
+            fp: entry.fp,
         }
     }
 }
 
-//TODO: QM31 reserved for instructions
 //TODO: add prev value
 #[derive(Debug, Default)]
 pub struct StateData {
     pub state: VmState,
-    pub memory_args: [(u32, QM31, u32, u32); 4], // (addr, val, prev_clock, clock)
+    pub memory_args: [(u32, QM31, QM31, u32, u32); 4], // (addr, val, prev_val, prev_clock, clock)
 }
 
 #[derive(Debug, Default)]
 pub struct StatesByOpcodes {
-    pub store_add_fp_fp: Vec<StateData>,          // [fp + off2] = [fp + off0] + [fp + off1]
-    pub store_add_fp_imm: Vec<StateData>,         // [fp + off2] = [fp + off0] + imm
-    pub store_sub_fp_fp: Vec<StateData>,          // [fp + off2] = [fp + off0] - [fp + off1]
-    pub store_sub_fp_imm: Vec<StateData>,         // [fp + off2] = [fp + off0] - imm
-    pub store_deref_fp: Vec<StateData>,           // [fp + off2] = [fp + off0]
-    pub store_double_deref_fp: Vec<StateData>,    // [fp + off2] = [[fp + off0] + off1]
-    pub store_imm: Vec<StateData>,                // [fp + off2] = imm
-    pub store_mul_fp_fp: Vec<StateData>,          // [fp + off2] = [fp + off0] * [fp + off1]
-    pub store_mul_fp_imm: Vec<StateData>,         // [fp + off2] = [fp + off0] * imm
-    pub store_div_fp_fp: Vec<StateData>,          // [fp + off2] = [fp + off0] / [fp + off1]
-    pub store_div_fp_imm: Vec<StateData>,         // [fp + off2] = [fp + off0] / imm
-    pub call_abs_fp: Vec<StateData>,              // call abs [fp + off0]
-    pub call_abs_imm: Vec<StateData>,             // call abs imm
-    pub call_rel_fp: Vec<StateData>,              // call rel [fp + off0]
-    pub call_rel_imm: Vec<StateData>,             // call rel imm
-    pub ret: Vec<StateData>,                      // ret
-    pub jmp_abs_add_fp_fp: Vec<StateData>,        // jmp abs [fp + off0] + [fp + off1]
-    pub jmp_abs_add_fp_imm: Vec<StateData>,       // jmp abs [fp + off0] + imm
-    pub jmp_abs_deref_fp: Vec<StateData>,         // jmp abs [fp + off0]
-    pub jmp_abs_double_deref_fp: Vec<StateData>,  // jmp abs [[fp + off0] + off1]
-    pub jmp_abs_imm: Vec<StateData>,              // jmp abs imm
-    pub jmp_abs_mul_fp_fp: Vec<StateData>,        // jmp abs [fp + off0] * [fp + off1]
-    pub jmp_abs_mul_fp_imm: Vec<StateData>,       // jmp abs [fp + off0] * imm
-    pub jmp_rel_add_fp_fp: Vec<StateData>,        // jmp rel [fp + off0] + [fp + off1]
-    pub jmp_rel_add_fp_imm: Vec<StateData>,       // jmp rel [fp + off0] + imm
-    pub jmp_rel_deref_fp: Vec<StateData>,         // jmp rel [fp + off0]
-    pub jmp_rel_double_deref_fp: Vec<StateData>,  // jmp rel [[fp + off0] + off1]
-    pub jmp_rel_imm: Vec<StateData>,              // jmp rel imm
-    pub jmp_rel_mul_fp_fp: Vec<StateData>,        // jmp rel [fp + off0] * [fp + off1]
-    pub jmp_rel_mul_fp_imm: Vec<StateData>,       // jmp rel [fp + off0] * imm
-    pub jnz_fp_fp: Vec<StateData>,                // jmp rel [fp + off1] if [fp + off0] != 0
+    pub store_add_fp_fp: Vec<StateData>, // [fp + off2] = [fp + off0] + [fp + off1]
+    pub store_add_fp_imm: Vec<StateData>, // [fp + off2] = [fp + off0] + imm
+    pub store_sub_fp_fp: Vec<StateData>, // [fp + off2] = [fp + off0] - [fp + off1]
+    pub store_sub_fp_imm: Vec<StateData>, // [fp + off2] = [fp + off0] - imm
+    pub store_deref_fp: Vec<StateData>,  // [fp + off2] = [fp + off0]
+    pub store_double_deref_fp: Vec<StateData>, // [fp + off2] = [[fp + off0] + off1]
+    pub store_imm: Vec<StateData>,       // [fp + off2] = imm
+    pub store_mul_fp_fp: Vec<StateData>, // [fp + off2] = [fp + off0] * [fp + off1]
+    pub store_mul_fp_imm: Vec<StateData>, // [fp + off2] = [fp + off0] * imm
+    pub store_div_fp_fp: Vec<StateData>, // [fp + off2] = [fp + off0] / [fp + off1]
+    pub store_div_fp_imm: Vec<StateData>, // [fp + off2] = [fp + off0] / imm
+    pub call_abs_fp: Vec<StateData>,     // call abs [fp + off0]
+    pub call_abs_imm: Vec<StateData>,    // call abs imm
+    pub call_rel_fp: Vec<StateData>,     // call rel [fp + off0]
+    pub call_rel_imm: Vec<StateData>,    // call rel imm
+    pub ret: Vec<StateData>,             // ret
+    pub jmp_abs_add_fp_fp: Vec<StateData>, // jmp abs [fp + off0] + [fp + off1]
+    pub jmp_abs_add_fp_imm: Vec<StateData>, // jmp abs [fp + off0] + imm
+    pub jmp_abs_deref_fp: Vec<StateData>, // jmp abs [fp + off0]
+    pub jmp_abs_double_deref_fp: Vec<StateData>, // jmp abs [[fp + off0] + off1]
+    pub jmp_abs_imm: Vec<StateData>,     // jmp abs imm
+    pub jmp_abs_mul_fp_fp: Vec<StateData>, // jmp abs [fp + off0] * [fp + off1]
+    pub jmp_abs_mul_fp_imm: Vec<StateData>, // jmp abs [fp + off0] * imm
+    pub jmp_rel_add_fp_fp: Vec<StateData>, // jmp rel [fp + off0] + [fp + off1]
+    pub jmp_rel_add_fp_imm: Vec<StateData>, // jmp rel [fp + off0] + imm
+    pub jmp_rel_deref_fp: Vec<StateData>, // jmp rel [fp + off0]
+    pub jmp_rel_double_deref_fp: Vec<StateData>, // jmp rel [[fp + off0] + off1]
+    pub jmp_rel_imm: Vec<StateData>,     // jmp rel imm
+    pub jmp_rel_mul_fp_fp: Vec<StateData>, // jmp rel [fp + off0] * [fp + off1]
+    pub jmp_rel_mul_fp_imm: Vec<StateData>, // jmp rel [fp + off0] * imm
+    pub jnz_fp_fp: Vec<StateData>,       // jmp rel [fp + off1] if [fp + off0] != 0
     pub jnz_fp_imm: Vec<StateData>,
 }
 
@@ -70,8 +69,13 @@ pub struct Instructions {
 }
 
 impl Instructions {
-    pub fn push_instr<I>(&mut self, mut memory: I, state: VmState, clock: u32, memory_cache: &mut MemoryCache)
-    where
+    pub fn push_instr<I>(
+        &mut self,
+        mut memory: I,
+        state: VmState,
+        clock: u32,
+        memory_cache: &mut MemoryCache,
+    ) where
         I: Iterator<Item = MemEntry>,
     {
         let mut state_data = StateData {
@@ -84,7 +88,8 @@ impl Instructions {
         state_data.memory_args[0] = memory_cache.push(instruction, clock);
 
         match opcode_id {
-            0 => { // store_add_fp_fp
+            0 => {
+                // store_add_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
@@ -93,14 +98,16 @@ impl Instructions {
                 state_data.memory_args[3] = memory_cache.push(mem3, clock);
                 self.states_by_opcodes.store_add_fp_fp.push(state_data);
             }
-            1 => { // store_add_fp_imm
+            1 => {
+                // store_add_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.store_add_fp_imm.push(state_data);
             }
-            2 => { // store_sub_fp_fp
+            2 => {
+                // store_sub_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
@@ -109,35 +116,42 @@ impl Instructions {
                 state_data.memory_args[3] = memory_cache.push(mem3, clock);
                 self.states_by_opcodes.store_sub_fp_fp.push(state_data);
             }
-            3 => { // store_sub_fp_imm
+            3 => {
+                // store_sub_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.store_sub_fp_imm.push(state_data);
             }
-            4 => { // store_deref_fp
+            4 => {
+                // store_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.store_deref_fp.push(state_data);
             }
-            5 => { // store_double_deref_fp
+            5 => {
+                // store_double_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 let mem3 = memory.next().unwrap();
                 state_data.memory_args[3] = memory_cache.push(mem3, clock);
-                self.states_by_opcodes.store_double_deref_fp.push(state_data);
+                self.states_by_opcodes
+                    .store_double_deref_fp
+                    .push(state_data);
             }
-            6 => { // store_imm
+            6 => {
+                // store_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.store_imm.push(state_data);
             }
-            7 => { // store_mul_fp_fp
+            7 => {
+                // store_mul_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
@@ -146,14 +160,16 @@ impl Instructions {
                 state_data.memory_args[3] = memory_cache.push(mem3, clock);
                 self.states_by_opcodes.store_mul_fp_fp.push(state_data);
             }
-            8 => { // store_mul_fp_imm
+            8 => {
+                // store_mul_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.store_mul_fp_imm.push(state_data);
             }
-            9 => { // store_div_fp_fp
+            9 => {
+                // store_div_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
@@ -162,123 +178,149 @@ impl Instructions {
                 state_data.memory_args[3] = memory_cache.push(mem3, clock);
                 self.states_by_opcodes.store_div_fp_fp.push(state_data);
             }
-            10 => { // store_div_fp_imm
+            10 => {
+                // store_div_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.store_div_fp_imm.push(state_data);
             }
-            11 => { // call_abs_fp
+            11 => {
+                // call_abs_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.call_abs_fp.push(state_data);
             }
-            12 => { // call_abs_imm
+            12 => {
+                // call_abs_imm
                 self.states_by_opcodes.call_abs_imm.push(state_data);
             }
-            13 => { // call_rel_fp
+            13 => {
+                // call_rel_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.call_rel_fp.push(state_data);
             }
-            14 => { // call_rel_imm
+            14 => {
+                // call_rel_imm
                 self.states_by_opcodes.call_rel_imm.push(state_data);
             }
-            15 => { // ret
+            15 => {
+                // ret
                 self.states_by_opcodes.ret.push(state_data);
             }
-            16 => { // jmp_abs_add_fp_fp
+            16 => {
+                // jmp_abs_add_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.jmp_abs_add_fp_fp.push(state_data);
             }
-            17 => { // jmp_abs_add_fp_imm
+            17 => {
+                // jmp_abs_add_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_abs_add_fp_imm.push(state_data);
             }
-            18 => { // jmp_abs_deref_fp
+            18 => {
+                // jmp_abs_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_abs_deref_fp.push(state_data);
             }
-            19 => { // jmp_abs_double_deref_fp
+            19 => {
+                // jmp_abs_double_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
-                self.states_by_opcodes.jmp_abs_double_deref_fp.push(state_data);
+                self.states_by_opcodes
+                    .jmp_abs_double_deref_fp
+                    .push(state_data);
             }
-            20 => { // jmp_abs_imm
+            20 => {
+                // jmp_abs_imm
                 self.states_by_opcodes.jmp_abs_imm.push(state_data);
             }
-            21 => { // jmp_abs_mul_fp_fp
+            21 => {
+                // jmp_abs_mul_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.jmp_abs_mul_fp_fp.push(state_data);
             }
-            22 => { // jmp_abs_mul_fp_imm
+            22 => {
+                // jmp_abs_mul_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_abs_mul_fp_imm.push(state_data);
             }
-            23 => { // jmp_rel_add_fp_fp
+            23 => {
+                // jmp_rel_add_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.jmp_rel_add_fp_fp.push(state_data);
             }
-            24 => { // jmp_rel_add_fp_imm
+            24 => {
+                // jmp_rel_add_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_rel_add_fp_imm.push(state_data);
             }
-            25 => { // jmp_rel_deref_fp
+            25 => {
+                // jmp_rel_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_rel_deref_fp.push(state_data);
             }
-            26 => { // jmp_rel_double_deref_fp
+            26 => {
+                // jmp_rel_double_deref_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
-                self.states_by_opcodes.jmp_rel_double_deref_fp.push(state_data);
+                self.states_by_opcodes
+                    .jmp_rel_double_deref_fp
+                    .push(state_data);
             }
-            27 => { // jmp_rel_imm
+            27 => {
+                // jmp_rel_imm
                 self.states_by_opcodes.jmp_rel_imm.push(state_data);
             }
-            28 => { // jmp_rel_mul_fp_fp
+            28 => {
+                // jmp_rel_mul_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.jmp_rel_mul_fp_fp.push(state_data);
             }
-            29 => { // jmp_rel_mul_fp_imm
+            29 => {
+                // jmp_rel_mul_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jmp_rel_mul_fp_imm.push(state_data);
             }
-            30 => { // jnz_fp_fp
+            30 => {
+                // jnz_fp_fp
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 let mem2 = memory.next().unwrap();
                 state_data.memory_args[2] = memory_cache.push(mem2, clock);
                 self.states_by_opcodes.jnz_fp_fp.push(state_data);
             }
-            31 => { // jnz_fp_imm
+            31 => {
+                // jnz_fp_imm
                 let mem1 = memory.next().unwrap();
                 state_data.memory_args[1] = memory_cache.push(mem1, clock);
                 self.states_by_opcodes.jnz_fp_imm.push(state_data);
-            }           
-            _ => panic!("Unknown opcode: {}", opcode_id),
+            }
+            _ => panic!("Unknown opcode: {opcode_id}"),
         }
     }
-} 
+}
--- crates/prover/src/adapter/memory.rs
+++ crates/prover/src/adapter/memory.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+
 use stwo_prover::core::fields::qm31::QM31;
 
 use super::io::MemEntry;
@@ -13,7 +14,7 @@ pub struct MemoryBoundaries {
 pub struct MemoryCache {
     clock_cache: HashMap<u32, u32>,
     initial_memory: HashMap<u32, [u32; 4]>,
-    final_memory: HashMap<u32, [u32; 4]>,
+    current_memory: HashMap<u32, [u32; 4]>,
 }
 
 impl MemoryCache {
@@ -21,21 +22,34 @@ impl MemoryCache {
         Self {
             clock_cache: HashMap::new(),
             initial_memory: HashMap::new(),
-            final_memory: HashMap::new(),
+            current_memory: HashMap::new(),
         }
     }
 
-    pub fn push(&mut self, mem_entry: MemEntry, clock: u32) -> (u32, QM31, u32, u32) {
-        let prev_clock = self.clock_cache.get(&mem_entry.addr).copied().unwrap_or_else(|| {
-            self.initial_memory.insert(mem_entry.addr, mem_entry.val);
-            0
-        });
+    pub fn push(&mut self, mem_entry: MemEntry, clock: u32) -> (u32, QM31, QM31, u32, u32) {
+        let prev_clock = self
+            .clock_cache
+            .get(&mem_entry.addr)
+            .copied()
+            .unwrap_or_else(|| {
+                self.initial_memory.insert(mem_entry.addr, mem_entry.val);
+                0
+            });
         self.clock_cache.insert(mem_entry.addr, clock);
-        self.final_memory.insert(mem_entry.addr, mem_entry.val);
-        
+        let old_value = self
+            .current_memory
+            .insert(mem_entry.addr, mem_entry.val)
+            .unwrap_or([0, 0, 0, 0]);
+
         (
             mem_entry.addr,
-            QM31::from_u32_unchecked(mem_entry.val[0], mem_entry.val[1], mem_entry.val[2], mem_entry.val[3]),
+            QM31::from_u32_unchecked(old_value[0], old_value[1], old_value[2], old_value[3]),
+            QM31::from_u32_unchecked(
+                mem_entry.val[0],
+                mem_entry.val[1],
+                mem_entry.val[2],
+                mem_entry.val[3],
+            ),
             prev_clock,
             clock,
         )
@@ -43,14 +57,16 @@ impl MemoryCache {
 
     pub fn get_memory_boundaries(&self) -> MemoryBoundaries {
         MemoryBoundaries {
-            initial_memory: self.initial_memory
+            initial_memory: self
+                .initial_memory
                 .iter()
                 .map(|(&addr, &val)| MemEntry { addr, val })
                 .collect(),
-            final_memory: self.final_memory
+            final_memory: self
+                .current_memory
                 .iter()
                 .map(|(&addr, &val)| MemEntry { addr, val })
                 .collect(),
         }
     }
-} 
+}
