4b7313b268a10473138767c9af218c8b460948a6
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-06-17 16:48:28 +0200

    first commit for adatper

---
+++ crates/prover/src/adapter/adapter.rs
@@ -0,0 +1,59 @@
+use std::path::Path;
+use tracing::{span, Level};
+
+use super::instructions::Instructions;
+use super::io::{read_memory_and_trace_from_paths, MemEntry, TraceEntry, VmImportError};
+use super::memory::{MemoryBoundaries, MemoryCache};
+
+pub struct ProverInput {
+    pub memory_boundaries: MemoryBoundaries,
+    pub instructions: Instructions,
+}
+
+pub fn import_from_vm_output(
+    trace_path: &Path,
+    mem_path: &Path,
+) -> Result<ProverInput, VmImportError> {
+    let _span = span!(Level::INFO, "import_from_vm_output").entered();
+
+    let (memory_entries, trace_entries) = read_memory_and_trace_from_paths(trace_path, mem_path)?;
+    
+    Ok(adapt_from_iter(memory_entries, trace_entries))
+}
+
+pub fn adapt_from_iter<I: IntoIterator<Item = MemEntry>, J: IntoIterator<Item = TraceEntry>>(
+    mem_iter: I,
+    trace_iter: J,
+) -> ProverInput {
+    let mut instructions = Instructions::default();
+    let mut memory = mem_iter.into_iter();
+    let mut trace = trace_iter.into_iter();
+    let mut clock = 1;
+    let mut memory_cache = MemoryCache::new();
+
+    let Some(first) = trace.next() else {
+        let memory: Vec<MemEntry> = memory.collect();
+        return ProverInput {
+            memory_boundaries: MemoryBoundaries {
+                initial_memory: memory.clone(),
+                final_memory: memory,
+            },
+            instructions,
+        };
+    };
+
+    instructions.initial_state = first.into();
+    instructions.push_instr(&mut memory, first.into(), clock, &mut memory_cache);
+    clock += 1;
+
+    for entry in trace {
+        instructions.final_state = entry.into();
+        instructions.push_instr(&mut memory, entry.into(), clock, &mut memory_cache);
+        clock += 1;
+    }
+    
+    ProverInput {
+        memory_boundaries: memory_cache.get_memory_boundaries(),
+        instructions,
+    }
+} 
---
+++ crates/prover/src/adapter/io.rs
@@ -0,0 +1,69 @@
+use std::io::Read;
+use std::path::Path;
+use bytemuck::{bytes_of_mut, Pod, Zeroable};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum VmImportError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("JSON error: {0}")]
+    Json(#[from] sonic_rs::Error),
+    #[error("No memory segments")]
+    NoMemorySegments,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, Pod, Zeroable)]
+pub struct TraceEntry {
+    pub pc: u32,
+    pub fp: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Default, Pod, Zeroable, Debug)]
+pub struct MemEntry {
+    pub addr: u32,
+    pub val: [u32; 4],
+}
+
+pub struct TraceIter<'a, R: Read>(pub &'a mut R);
+
+impl<R: Read> Iterator for TraceIter<'_, R> {
+    type Item = TraceEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut entry = TraceEntry::default();
+        self.0
+            .read_exact(bytes_of_mut(&mut entry))
+            .ok()
+            .map(|_| entry)
+    }
+}
+
+pub struct MemEntryIter<'a, R: Read>(pub &'a mut R);
+
+impl<R: Read> Iterator for MemEntryIter<'_, R> {
+    type Item = MemEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut entry = MemEntry::default();
+        self.0
+            .read_exact(bytes_of_mut(&mut entry))
+            .ok()
+            .map(|_| entry)
+    }
+}
+
+pub fn read_memory_and_trace_from_paths(
+    trace_path: &Path,
+    mem_path: &Path,
+) -> Result<(Vec<MemEntry>, Vec<TraceEntry>), VmImportError> {
+    let mut trace_file = std::io::BufReader::new(std::fs::File::open(trace_path)?);
+    let mut mem_file = std::io::BufReader::new(std::fs::File::open(mem_path)?);
+    
+    let memory_entries: Vec<MemEntry> = MemEntryIter(&mut mem_file).collect();
+    let trace_entries: Vec<TraceEntry> = TraceIter(&mut trace_file).collect();
+    
+    Ok((memory_entries, trace_entries))
+} 
