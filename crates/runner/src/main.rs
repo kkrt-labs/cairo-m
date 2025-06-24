1ff8945ae6f8a93ec9d0cb5beb4f0a835d1228cd
Oba <obatirou@gmail.com>
2025-06-24 11:37:22 +0200

    feat(vm): use anyhow for CLI (#94)

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -1,6 +1,7 @@
+use std::fs;
 use std::path::PathBuf;
-use std::{fs, process};
 
+use anyhow::{Context, Error, Result};
 use cairo_m_common::Program;
 use cairo_m_runner::run_cairo_program;
 use clap::{Parser, ValueHint};
@@ -26,28 +27,19 @@ struct Args {
     verbose: bool,
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let file_content = fs::read_to_string(&args.compiled_file).unwrap_or_else(|e| {
-        eprintln!(
-            "Error reading file '{}': {}",
-            args.compiled_file.display(),
-            e
-        );
-        process::exit(1);
-    });
+    let file_content = fs::read_to_string(&args.compiled_file)
+        .with_context(|| format!("Error reading file '{}'", args.compiled_file.display()))?;
 
-    let compiled_program: Program = sonic_rs::from_str(&file_content).unwrap_or_else(|e| {
-        eprintln!("Failed to parse compiled program: {}", e);
-        process::exit(1);
-    });
+    let compiled_program: Program =
+        sonic_rs::from_str(&file_content).context("Failed to parse compiled program")?;
 
     let output = run_cairo_program(&compiled_program, &args.entrypoint, Default::default())
-        .unwrap_or_else(|e| {
-            eprintln!("Execution failed: {}", e);
-            process::exit(1);
-        });
+        .context("Execution failed")?;
 
     println!("Run succeeded and returned: [{}]", output.return_value);
+
+    Ok(())
 }
