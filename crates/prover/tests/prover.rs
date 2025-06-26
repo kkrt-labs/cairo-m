0f87ce5c3a6fed95e75c7d45d475a6bd944c3953
Oba <obatirou@gmail.com>
2025-06-26 12:20:52 +0200

    profile

--- crates/prover/tests/prover.rs
+++ crates/prover/tests/prover.rs
@@ -13,6 +13,10 @@ use stwo_prover::core::fields::m31::M31;
 use stwo_prover::core::fields::qm31::QM31;
 use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleChannel;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 #[test]
 fn test_prove_and_verify_unchanged_memory() {
     let initial_memory_data = [
@@ -113,7 +117,6 @@ fn test_prove_and_verify_recursive_fibonacci_program() {
         Default::default(),
     )
     .unwrap();
-
     let mut prover_input = import_from_runner_output(&runner_output).unwrap();
     let proof = prove_cairo_m::<Blake2sMerkleChannel>(&mut prover_input).unwrap();
 
@@ -144,4 +147,34 @@ fn test_all_constraints() {
 
     let mut prover_input = import_from_runner_output(&runner_output).unwrap();
     assert_constraints(&mut prover_input);
+}
+
+#[cfg(feature = "dhat-heap")]
+#[test]
+fn test_memory_profile_fibonacci_prover() {
+    let source_path = format!(
+        "{}/tests/test_data/{}",
+        env!("CARGO_MANIFEST_DIR"),
+        "fibonacci.cm"
+    );
+    let compiled_fib = compile_cairo(
+        fs::read_to_string(&source_path).unwrap(),
+        source_path,
+        CompilerOptions::default(),
+    )
+    .unwrap();
+
+    let runner_output = run_cairo_program(
+        &compiled_fib.program,
+        "fib",
+        &[M31::from(100000)],
+        Default::default(),
+    )
+    .unwrap();
+
+    let _profiler = dhat::Profiler::new_heap();
+
+    let mut prover_input = import_from_runner_output(&runner_output).unwrap();
+    let _proof: cairo_m_prover::Proof<stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher> =
+        prove_cairo_m::<Blake2sMerkleChannel>(&mut prover_input).unwrap();
 }
