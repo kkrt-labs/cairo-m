5d6a4a9f384dfa2895c54c7d1277fa5c690282c7
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 16:54:05 +0000

    add Cairo-M Prover Memory Benchmarks (customBiggerIsBetter) benchmark result for 99f983c71dfe973efa9a434556ecb2dca174a1c0

--- dev/bench-prover-mem/data.js
+++ dev/bench-prover-mem/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751039794150,
+  "lastUpdate": 1751043245872,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Memory Benchmarks": [
@@ -81,6 +81,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kkrt-labs/cairo-m/commit/874c88d63b6fb0d20184e9dfb34624d50d3d5102"
         },
         "date": 1751039794113,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "fibonacci_prove_peak_mem",
+            "value": 2428608464,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "66871571+Eikix@users.noreply.github.com",
+            "name": "Elias Tazartes",
+            "username": "Eikix"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "99f983c71dfe973efa9a434556ecb2dca174a1c0",
+          "message": "rebase stwo (#129)",
+          "timestamp": "2025-06-27T18:41:14+02:00",
+          "tree_id": "db8cc53d2db718c9d12ee6257f47088eed09abda",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/99f983c71dfe973efa9a434556ecb2dca174a1c0"
+        },
+        "date": 1751043245832,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
