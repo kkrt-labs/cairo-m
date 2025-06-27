ab2e4f03a91126de874ed4ff3909a6996f74f7d8
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 09:54:19 +0000

    add Cairo-M VM Benchmarks (cargo) benchmark result for 7cc842c1b192964ce97d5a16fa6998cfee105992

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750928599308,
+  "lastUpdate": 1751018059625,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M VM Benchmarks": [
@@ -1003,6 +1003,36 @@ window.BENCHMARK_DATA = {
             "name": "fibonacci_1m/execution_only",
             "value": 258038115,
             "range": "± 1440579",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60658558+enitrat@users.noreply.github.com",
+            "name": "Mathieu",
+            "username": "enitrat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7cc842c1b192964ce97d5a16fa6998cfee105992",
+          "message": "ci: add benchmarks on prover crate (#121)\n\n* dev: add benchmarks on prover\n\n* fix ghpage issue\n\n* fixes\n\n* updat n_iter to 100k\n\n* count vm adapter in benchmark",
+          "timestamp": "2025-06-27T11:51:29+02:00",
+          "tree_id": "9c35e2405045debf9490749a5bf126b4e3c39888",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/7cc842c1b192964ce97d5a16fa6998cfee105992"
+        },
+        "date": 1751018059095,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fibonacci_1m/execution_only",
+            "value": 259217700,
+            "range": "± 766646",
             "unit": "ns/iter"
           }
         ]
