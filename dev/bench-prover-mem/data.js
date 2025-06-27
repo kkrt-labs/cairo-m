abb6b2d33718869d58a16057d02f3e7640adef5f
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 10:00:10 +0000

    add Cairo-M Prover Memory Benchmarks (customBiggerIsBetter) benchmark result for 7cc842c1b192964ce97d5a16fa6998cfee105992

---
+++ dev/bench-prover-mem/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1751018410612,
+  "repoUrl": "https://github.com/kkrt-labs/cairo-m",
+  "entries": {
+    "Cairo-M Prover Memory Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "60658558+enitrat@users.noreply.github.com",
+            "name": "Mathieu",
+            "username": "enitrat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7cc842c1b192964ce97d5a16fa6998cfee105992",
+          "message": "ci: add benchmarks on prover crate (#121)\n\n* dev: add benchmarks on prover\n\n* fix ghpage issue\n\n* fixes\n\n* updat n_iter to 100k\n\n* count vm adapter in benchmark",
+          "timestamp": "2025-06-27T11:51:29+02:00",
+          "tree_id": "9c35e2405045debf9490749a5bf126b4e3c39888",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/7cc842c1b192964ce97d5a16fa6998cfee105992"
+        },
+        "date": 1751018410576,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "fibonacci_prove_peak_mem",
+            "value": 1964610240,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
