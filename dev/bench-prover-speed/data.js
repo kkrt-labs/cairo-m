a2374ea817820de2fa1d0d9a9181e70eb30494b4
github-action-benchmark <github@users.noreply.github.com>
2025-07-07 15:27:41 +0000

    add Cairo-M Prover Speed Benchmarks (cargo) benchmark result for e3bd6a375d386b2817e9591e156cc87f75b2b8b8

--- dev/bench-prover-speed/data.js
+++ dev/bench-prover-speed/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751902053703,
+  "lastUpdate": 1751902061218,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Speed Benchmarks": [
@@ -209,6 +209,36 @@ window.BENCHMARK_DATA = {
             "name": "prover_fibonacci/prove",
             "value": 5650078065,
             "range": "± 29247178",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement0walter@gmail.com",
+            "name": "Clément Walter",
+            "username": "ClementWalter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e3bd6a375d386b2817e9591e156cc87f75b2b8b8",
+          "message": "soundness(prover): Split relation column using mul (#128)\n\n* Split rel column using op0 * op1\n\n* Split rel column using op0 * imm\n\n* Split rel column using op0 * imm in jmp_abs_mul_fp_fp\n\n* Split rel column using op0 * imm in jmp_rel_mul_fp_fp\n\n* Split rel column using op0 * imm in jmp_abs_mul_fp_imm\n\n* Split rel column using op0 * imm in jmp_rel_mul_fp_imm",
+          "timestamp": "2025-07-07T17:14:48+02:00",
+          "tree_id": "6d1bc4d35b134f5fa8e228e269c7131d7bd8a4f9",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/e3bd6a375d386b2817e9591e156cc87f75b2b8b8"
+        },
+        "date": 1751902060723,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "prover_fibonacci/prove",
+            "value": 5659134728,
+            "range": "± 26870140",
             "unit": "ns/iter"
           }
         ]
