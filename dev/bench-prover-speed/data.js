0b080dae15fa7f336a25a283f784ff51e70c1669
github-action-benchmark <github@users.noreply.github.com>
2025-07-09 13:21:42 +0000

    add Cairo-M Prover Speed Benchmarks (cargo) benchmark result for 053338b3824c34d78db60933c9dee397f6a0c0bf

--- dev/bench-prover-speed/data.js
+++ dev/bench-prover-speed/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752060647246,
+  "lastUpdate": 1752067302723,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Speed Benchmarks": [
@@ -329,6 +329,36 @@ window.BENCHMARK_DATA = {
             "name": "prover_fibonacci/prove",
             "value": 5678419054,
             "range": "± 29754072",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "antoine.fondeur@gmail.com",
+            "name": "Antoine Fondeur",
+            "username": "AntoineFONDEUR"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "053338b3824c34d78db60933c9dee397f6a0c0bf",
+          "message": "feat(prover): prepare inputs to Merkle component (#139)\n\n* non optimal memory-wise merkle tree building\n\n* remove out.txt file\n\n* updated memory boundaries, hash on M31 and add hash as generic\n\n* switch to depth rather than layer\n\n* remove root hash from node data vec",
+          "timestamp": "2025-07-09T15:09:23+02:00",
+          "tree_id": "8fd081290d9c37989cdf61e6cd11b73cdfc9dfbb",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/053338b3824c34d78db60933c9dee397f6a0c0bf"
+        },
+        "date": 1752067301696,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "prover_fibonacci/prove",
+            "value": 5622880833,
+            "range": "± 35887480",
             "unit": "ns/iter"
           }
         ]
