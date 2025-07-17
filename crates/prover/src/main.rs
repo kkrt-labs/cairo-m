fb12be98e385d392af128555aa42fa05afdb548b
Mathieu <60658558+enitrat@users.noreply.github.com>
2025-07-17 11:32:57 +0100

    feat(prover): dump proof output (#168)
    * feat(prover): dump proof output
    
    * feat(prover): dump proof output to json file
    
    * recs

--- crates/prover/src/main.rs
+++ crates/prover/src/main.rs
@@ -5,6 +5,7 @@ use anyhow::{Context, Error, Result};
 use cairo_m_common::Program;
 use cairo_m_prover::adapter::import_from_runner_output;
 use cairo_m_prover::prover::prove_cairo_m;
+use cairo_m_prover::verifier::verify_cairo_m;
 use cairo_m_runner::run_cairo_program;
 use clap::{Parser, ValueHint};
 use stwo_prover::core::fields::m31::M31;
@@ -29,6 +30,14 @@ struct Args {
     /// Arguments to pass to the entrypoint
     #[arg(short, long)]
     arguments: Vec<u32>,
+
+    /// Output file to write the proof to
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+
+    /// Whether to verify the proof
+    #[arg(short, long, default_value = "false")]
+    verify: bool,
 
     /// Enable verbose output
     #[arg(short, long)]
@@ -55,8 +64,21 @@ fn main() -> Result<(), Error> {
 
     let mut prover_input =
         import_from_runner_output(output).context("Failed to import from runner output")?;
-    let _proof: cairo_m_prover::Proof<stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher> =
-        prove_cairo_m::<Blake2sMerkleChannel>(&mut prover_input, None).context("Failed to prove")?;
+    let proof: cairo_m_prover::Proof<stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher> =
+        prove_cairo_m::<Blake2sMerkleChannel>(&mut prover_input, None)
+            .context("Failed to prove")?;
+
+    if let Some(output) = args.output {
+        let proof_output =
+            sonic_rs::to_string(&proof).context("Failed to serialize proof to JSON")?;
+        fs::write(&output, proof_output)?;
+        println!("Proof written to {}", output.display());
+    }
+
+    if args.verify {
+        verify_cairo_m::<Blake2sMerkleChannel>(proof, None).context("Failed to verify proof")?;
+        println!("Proof verified successfully!");
+    }
 
     Ok(())
 }
