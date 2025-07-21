2503a4cbc6c71ea040a830afb85ef44c2c9d0b54
Oba <obatirou@gmail.com>
2025-07-21 11:58:40 +0200

    feat(common): variable instructions size (#178)
    * feat(common): variable instructions size
    
    * remove StoreDerefFp
    
    * adds comments for define_instruction

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,10 +1,8 @@
 pub mod execution;
 pub mod instruction;
-pub mod opcode;
 pub mod program;
 pub mod state;
 
-pub use instruction::Instruction;
-pub use opcode::Opcode;
+pub use instruction::{Instruction, InstructionError};
 pub use program::{Program, ProgramMetadata};
 pub use state::State;
