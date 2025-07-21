0dd74ae71b2d1927e2abf79f5849e657a55a0850
github-action-benchmark <github@users.noreply.github.com>
2025-07-21 14:11:38 +0000

    add Cairo-M Prover Speed Benchmarks (cargo) benchmark result for 9a9b47179d84b13b29016be0cea9ae8638d75c15

--- dev/bench-prover-speed/data.js
+++ dev/bench-prover-speed/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753092861721,
+  "lastUpdate": 1753107098718,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Speed Benchmarks": [
@@ -719,6 +719,36 @@ window.BENCHMARK_DATA = {
             "name": "prover_fibonacci/prove",
             "value": 5697373144,
             "range": "± 17943479",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement0walter@gmail.com",
+            "name": "Clément Walter",
+            "username": "ClementWalter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9a9b47179d84b13b29016be0cea9ae8638d75c15",
+          "message": "feat(prover): Consolidate JnzFpImm opcode implementation (#175)\n\n* Vibecode it\n\n* Add PR claude command\n\n* Make sure that taken is the bool version of op0 * op0_inv\n\n* Add pc_new col to save one interaction",
+          "timestamp": "2025-07-21T15:59:13+02:00",
+          "tree_id": "4435311b9a662c38c2d7041ccbb9d535aac113d8",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/9a9b47179d84b13b29016be0cea9ae8638d75c15"
+        },
+        "date": 1753107098262,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "prover_fibonacci/prove",
+            "value": 5673430501,
+            "range": "± 22105900",
             "unit": "ns/iter"
           }
         ]
