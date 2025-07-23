9c088f3a432d610b8e6931a1aa5b246c3e5d2d2f
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-07-23 10:21:37 +0200

    fixed typo for public addresses

--- crates/runner/src/lib.rs
+++ crates/runner/src/lib.rs
@@ -114,7 +114,7 @@ pub fn run_cairo_program(
     }
 
     // For now the public memory addresses enables the verifier to know the end-of-execution content of given addresses
-    let end_addr = vm.state.fp + M31::from(2);
+    let end_addr = vm.state.fp - M31::from(2);
     let public_addresses = (0..end_addr.0).map(M31::from).collect();
 
     Ok(RunnerOutput {
