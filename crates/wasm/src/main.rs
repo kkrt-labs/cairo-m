40e92989aaa473e058e71a33888d0bb7f87efae7
Gael Bigot <gbigotraingeard@gmail.com>
2025-08-05 11:54:26 +0200

    improved main

--- crates/wasm/src/main.rs
+++ crates/wasm/src/main.rs
@@ -1,9 +1,23 @@
 mod loader;
 
 use loader::{load_module, print_womir_program};
+use std::env;
 
 fn main() {
-    let program = load_module("tests/test_cases/add.wasm").unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    print_womir_program(&program);
+    if args.len() != 2 {
+        eprintln!("Usage: {} <wasm_file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    match load_module(filename) {
+        Ok(program) => print_womir_program(&program),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
