d5b41bd9591caa8eb3b91f892f6c2a4524b8d8e5
enitrat <msaug@protonmail.com>
2025-09-02 10:14:01 +0100

    fix conflicts of rebase

--- crates/prover/src/components/opcodes/u32_store_eq_fp_fp.rs
+++ crates/prover/src/components/opcodes/u32_store_eq_fp_fp.rs
@@ -75,9 +75,10 @@ use stwo_prover::core::pcs::TreeVec;
 use stwo_prover::core::poly::circle::CircleEvaluation;
 use stwo_prover::core::poly::BitReversedOrder;
 
-use crate::adapter::ExecutionBundle;
+use crate::adapter::{memory::DataAccess, ExecutionBundle};
 use crate::components::Relations;
 use crate::preprocessed::range_check::RangeCheckProvider;
+use crate::utils::access_log::{get_prev_clock, get_prev_value, get_value};
 use crate::utils::enabler::Enabler;
 use crate::utils::execution_bundle::PackedExecutionBundle;
 
@@ -141,6 +142,7 @@ impl Claim {
     /// after being packed into SIMD-friendly format.
     pub fn write_trace<MC: MerkleChannel>(
         inputs: &mut Vec<ExecutionBundle>,
+        data_accesses: &[DataAccess],
     ) -> (Self, ComponentTrace<N_TRACE_COLUMNS>, InteractionClaimData)
     where
         SimdBackend: BackendForChannel<MC>,
@@ -191,18 +193,18 @@ impl Claim {
                 let src1_off = input.inst_value_2;
                 let dst_off = input.inst_value_4;
 
-                let op0_val_lo = input.mem1_value;
-                let op0_val_hi = input.mem2_value;
-                let op0_prev_lo_clock = input.mem1_prev_clock;
-                let op0_prev_hi_clock = input.mem2_prev_clock;
+                let op0_val_lo = get_value(input, data_accesses, 0);
+                let op0_val_hi = get_value(input, data_accesses, 1);
+                let op0_prev_lo_clock = get_prev_clock(input, data_accesses, 0);
+                let op0_prev_hi_clock = get_prev_clock(input, data_accesses, 1);
 
-                let op1_val_lo = input.mem3_value;
-                let op1_val_hi = input.mem4_value;
-                let op1_prev_lo_clock = input.mem3_prev_clock;
-                let op1_prev_hi_clock = input.mem4_prev_clock;
+                let op1_val_lo = get_value(input, data_accesses, 2);
+                let op1_val_hi = get_value(input, data_accesses, 3);
+                let op1_prev_lo_clock = get_prev_clock(input, data_accesses, 2);
+                let op1_prev_hi_clock = get_prev_clock(input, data_accesses, 3);
 
-                let dst_prev_val = input.mem5_prev_value;
-                let dst_prev_clock = input.mem5_prev_clock;
+                let dst_prev_val = get_prev_value(input, data_accesses, 4);
+                let dst_prev_clock = get_prev_clock(input, data_accesses, 4);
 
                 let diff =
                     op1_val_lo + op1_val_hi * two_pow_16 - op0_val_lo - op0_val_hi * two_pow_16;
