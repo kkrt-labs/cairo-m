8376449b1b8e31e79d3ad1de610175644ff16288
github-action-benchmark <github@users.noreply.github.com>
2025-09-02 10:35:45 +0000

    add Cairo-M Prover Memory Benchmarks (customBiggerIsBetter) benchmark result for c363e4bf0a91ca814442dce190de7e9cd9f462c6

--- dev/bench-prover-mem/data.js
+++ dev/bench-prover-mem/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756738811394,
+  "lastUpdate": 1756809344992,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Memory Benchmarks": [
@@ -1449,6 +1449,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "fibonacci_prove_peak_mem",
             "value": 2440000007,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60658558+enitrat@users.noreply.github.com",
+            "name": "Mathieu",
+            "username": "enitrat"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c363e4bf0a91ca814442dce190de7e9cd9f462c6",
+          "message": "refactor(prover): refactor instructions operand accesses (#288)\n\n* refactor(prover): extract memory access tracking into dedicated module\n\n- Create dedicated AccessLog module for memory access tracking\n- Move memory access logic from ExecutionBundle to AccessLog\n- Simplify ExecutionBundle by delegating to AccessLog\n- Fix memory operations to use new access tracking system\n- Update all opcode components to use new memory interface\n\nThis refactoring improves code organization by separating concerns:\nmemory access tracking is now handled independently from execution\nbundle management, making the codebase more maintainable.\n\n* fix conflicts of rebase\n\n* some cleanup",
+          "timestamp": "2025-09-02T12:20:06+02:00",
+          "tree_id": "a9453d45310b375d7df55bc46486cfb54a7a94f0",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/c363e4bf0a91ca814442dce190de7e9cd9f462c6"
+        },
+        "date": 1756809344933,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "fibonacci_prove_peak_mem",
+            "value": 2377096455,
             "unit": "bytes"
           }
         ]
