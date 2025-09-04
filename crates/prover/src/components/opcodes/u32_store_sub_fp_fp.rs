274e2e14eb4e00c59ca8b616769d8dc087af285e
Antoine Fondeur <antoine.fondeur@gmail.com>
2025-09-04 14:39:38 +0200

    Update crates/prover/src/components/opcodes/u32_store_sub_fp_fp.rs

--- crates/prover/src/components/opcodes/u32_store_sub_fp_fp.rs
+++ crates/prover/src/components/opcodes/u32_store_sub_fp_fp.rs
@@ -46,8 +46,8 @@
 //!   * `- [fp + src0_off + 1, op0_prev_clk_hi, op0_val_hi] + [fp + src0_off + 1, clk, op0_val_hi]`
 //!   * `- [clk - op0_prev_lo_clk - 1]` and `- [clk - op0_prev_hi_clk - 1]` in `RangeCheck20` relation
 //! * read op1
-//!   * `- [fp + src1_off, op1_prev_clk, op1_val_lo] + [fp + src1_off, clk, op1_val_lo]`
-//!   * `- [fp + src1_off + 1, op1_prev_clk, op1_val_hi] + [fp + src1_off + 1, clk, op1_val_hi]`
+//!   * `- [fp + src1_off, op1_prev_clk_lo, op1_val_lo] + [fp + src1_off, clk, op1_val_lo]`
+//!   * `- [fp + src1_off + 1, op1_prev_clk_hi, op1_val_hi] + [fp + src1_off + 1, clk, op1_val_hi]`
 //!   * `- [clk - op1_prev_lo_clk - 1]` and `- [clk - op1_prev_hi_clk - 1]` in `RangeCheck20` relation
 //! * write dst in [fp + dst_off]
 //!   * `- [fp + dst_off, dst_prev_clk, dst_prev_val_lo] + [fp + dst_off, clk, op0_val_lo - borrow_lo << 16 - op1_val_lo]` in `Memory` Relation
