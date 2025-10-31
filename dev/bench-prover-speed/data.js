992d7586fdc1116ef45d3cce202255de51d0864a
github-action-benchmark <github@users.noreply.github.com>
2025-10-31 16:19:48 +0000

    add Cairo-M Prover Speed Benchmarks (cargo) benchmark result for 81ae2e2a97a88917b00e9e41f063d31b44bc85ad

--- dev/bench-prover-speed/data.js
+++ dev/bench-prover-speed/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760611263519,
+  "lastUpdate": 1761927588405,
   "repoUrl": "https://github.com/kkrt-labs/cairo-m",
   "entries": {
     "Cairo-M Prover Speed Benchmarks": [
@@ -2111,6 +2111,36 @@ window.BENCHMARK_DATA = {
             "name": "prover_fibonacci/prove",
             "value": 2824850497,
             "range": "± 32178653",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "clement0walter@gmail.com",
+            "name": "Clément Walter",
+            "username": "ClementWalter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "81ae2e2a97a88917b00e9e41f063d31b44bc85ad",
+          "message": "Run trunk fmt --all (#353)",
+          "timestamp": "2025-10-31T18:02:28+02:00",
+          "tree_id": "5eee1d95ad8699cc5da06ec28b9a33a1f072397e",
+          "url": "https://github.com/kkrt-labs/cairo-m/commit/81ae2e2a97a88917b00e9e41f063d31b44bc85ad"
+        },
+        "date": 1761927587245,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "prover_fibonacci/prove",
+            "value": 2848831198,
+            "range": "± 15225422",
             "unit": "ns/iter"
           }
         ]
